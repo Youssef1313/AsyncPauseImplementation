9957a1e180df7c8343fe079403bf0cc133aa4aa5
Youssef Victor <youssefvictor00@gmail.com>
2019-07-10 10:30:30 +0200

    Initial commit

--- AsyncPauseImplementation/Form1.Designer.cs
+++ AsyncPauseImplementation/Form1.Designer.cs
@@ -28,12 +28,77 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnPause = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox1.Location = new System.Drawing.Point(12, 1);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(754, 440);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnStart.Location = new System.Drawing.Point(12, 447);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnPause
+            // 
+            this.btnPause.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnPause.Enabled = false;
+            this.btnPause.Location = new System.Drawing.Point(111, 447);
+            this.btnPause.Name = "btnPause";
+            this.btnPause.Size = new System.Drawing.Size(75, 23);
+            this.btnPause.TabIndex = 2;
+            this.btnPause.Text = "Pause";
+            this.btnPause.UseVisualStyleBackColor = true;
+            // 
+            // btnStop
+            // 
+            this.btnStop.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnStop.Enabled = false;
+            this.btnStop.Location = new System.Drawing.Point(220, 447);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 3;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(778, 482);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnPause);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnPause;
+        private System.Windows.Forms.Button btnStop;
     }
 }
 
--- AsyncPauseImplementation/Form1.cs
+++ AsyncPauseImplementation/Form1.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,6 +17,43 @@ namespace AsyncPauseImplementation
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private async void btnStart_Click(object sender, EventArgs e)
+        {
+            btnStart.Enabled = false;
+            btnPause.Enabled = true;
+            btnStop.Enabled = true;
+
+            AddToLog("Program started.");
+
+            File.WriteAllLines("D:\\urls.txt", new []{ "https://google.com" , "https://google.com" , "https://google.com" , "https://google.com" });
+
+            var urls = File.ReadAllLines("D:\\urls.txt");
+            for (var i = 0; i < urls.Length; i++)
+            {
+                // ToDo: Check if user requested to pause operation, change button text to resume, wait here until user click resume and back the button text to pause.
+                var result = await DownloadContentAsync(urls[i]).ConfigureAwait(true); // Because next lines are interacting with UI. (Thanks for your video ^_^)
+                File.WriteAllText($"D:\\{i}.txt", result);
+                AddToLog($"Downloaded web contents for {urls[i]}");
+            }
+
+            AddToLog("Program finished.");
+
+            btnStart.Enabled = false;
+            btnPause.Enabled = true;
+            btnStop.Enabled = true;
+
+        }
+
+        private static async Task<string> DownloadContentAsync(string url)
+        {
+            return await new WebClient().DownloadStringTaskAsync(url).ConfigureAwait(false);
+        }
+
+        private void AddToLog(string s)
+        {
+            richTextBox1.AppendText($"[{DateTime.Now.ToShortTimeString()}]: {s}{Environment.NewLine}");
         }
     }
 }
